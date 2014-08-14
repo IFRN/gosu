e67fdb27132a4874fd5ce7b13bb00b9588030ea7
Leonardo Lucena <leonardo@tads.projetos.ifrn.edu.br>
2014-08-14 14:25:13 -0300

    Update cogu.rb

--- 20132/crazycrash/cogu.rb
+++ 20132/crazycrash/cogu.rb
@@ -1,11 +1,11 @@
 class Cogu
   attr_reader :x, :y
 
-	def initialize(imagem, x, y)
-		@imagem = imagem
-		@x, @y = x, y
-	end   
-	def draw
-		@imagem.draw(@x, @y, 4, 1)
-	end   
+  def initialize(imagem, x, y)
+    @imagem = imagem
+    @x, @y = x, y
+  end   
+  def draw
+    @imagem.draw(@x, @y, 4, 1)
+  end   
 end
